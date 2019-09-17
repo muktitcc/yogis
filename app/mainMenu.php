305da82352828b5f02208c4e6495335ae98d1aa3
Mukti Nath Chhetri <muktitcc@users.noreply.github.com>
2019-09-17 10:03:53 +0600

    Update mainMenu.php

--- app/mainMenu.php
+++ app/mainMenu.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once("userAuthentication.php");
 require_once("mhvMenu_fn.php");
 $fn=new common_Functions();
