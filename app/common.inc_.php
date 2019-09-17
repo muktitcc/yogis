98eae205d7605209cb79a44f1e551fff9536505b
Mukti Nath Chhetri <muktitcc@users.noreply.github.com>
2019-09-17 10:08:48 +0600

    Update common.inc_.php

--- app/common.inc_.php
+++ app/common.inc_.php
@@ -1,5 +1,7 @@
 <?php
-error_reporting(1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once( "config_.php" );
 require_once( "config.php" );
 require_once( "configPDO.php" );
