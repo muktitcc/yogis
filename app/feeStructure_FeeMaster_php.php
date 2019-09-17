aa487dfd8da40311bdaa08d964f9c4279350bc06
Mukti Nath Chhetri <muktitcc@users.noreply.github.com>
2019-09-17 15:40:30 +0600

    Update feeStructure_FeeMaster_php.php

--- app/feeStructure_FeeMaster_php.php
+++ app/feeStructure_FeeMaster_php.php
@@ -1,6 +1,6 @@
 <?php
 include(PHP_JQGRID_CLASS_PATH_NEW."/jqgrid_dist.php");
-$fn_this=new feeStructure_FeeMaster(new common_Functions(),new jqgrid());
-$g = new jqgrid(); 
+$fn_this=new feeStructure_FeeMaster(new common_Functions(),new jqgrid($db_conf));
+$g = new jqgrid($db_conf); 
 $outFeeStructure = $fn_this->getFeeStructureGrid($g);
 ?>
