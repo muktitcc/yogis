b1a85b3c8744bde03d970ebddb3e9cdffc1ba2da
Mukti Nath Chhetri <muktitcc@users.noreply.github.com>
2019-09-17 10:00:19 +0600

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,6 +1,6 @@
 <?php
 require_once( "common.inc_.php" );
-//$conn = mysql_connect(DB_HOST, DB_USERNAME,DB_PASSWORD) or die ("Error connecting to database");
-//mysql_select_db(DB_NAME,$conn);
+$conn = mysql_connect(DB_HOST, DB_USERNAME,DB_PASSWORD) or die ("Error connecting to database");
+mysql_select_db(DB_NAME,$conn);
 
 ?>
