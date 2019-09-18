2652a6a49534bcb18ff7963f8b5cf7a33e38024a
Mukti Nath Chhetri <muktitcc@users.noreply.github.com>
2019-09-18 11:55:57 +0600

    Update registration_StudentRegistration_fn.php

--- app/registration_StudentRegistration_fn.php
+++ app/registration_StudentRegistration_fn.php
@@ -628,9 +628,9 @@ unlink( realpath(APPDATA_DIR.$data['newFile']));
 
 $needThumb="";
 if($data['newfileThumb']){
-  phpgrid_error("mm".$data['imagelocation']);  
-$needThumb=$this->fn->_getThumbnailImage(,APPDATA_DIR.$data['newfileThumb']);
-
+phpgrid_error("mm".realpath($data['imagelocation']));    
+$needThumb=$this->fn->_getThumbnailImage(realpath($data['imagelocation']),APPDATA_DIR.$data['newfileThumb']);
+phpgrid_error("mmmff");
 }else{
 $needThumb="ok";	
 }
